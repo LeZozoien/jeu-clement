4571be2c0a67e037358fd75043dabc127a0b11fe
ayabusa <lebgpub@gmail.com>
2023-10-04 18:31:01 +0200

    fix orientation personnage

--- Assets/le personnage/Controller.cs
+++ Assets/le personnage/Controller.cs
@@ -20,14 +20,17 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // stock les entrées du joueur
         float h = Input.GetAxis("Horizontal");
         float V = Input.GetAxis("Vertical");
-        float angle_du_vect = Mathf.Atan2(h, V) * Mathf.Rad2Deg / 360;
-        angle_du_vect = angle_du_vect + 0.5f;
-
+        
+        // vérifie si une touche est pressé avant de changer la direction du joueur
+        // évite le bug où le joueur finit tjr dans la même direction
+        if(h != 0f || V != 0){
+            float angle_du_vect = Mathf.Atan2(h, V) * Mathf.Rad2Deg / 360;
+            angle_du_vect = angle_du_vect + 0.5f;
+            animator.SetFloat("Angle", angle_du_vect);
+        }
         rb2d.velocity = new Vector2(h, V).normalized * speed;
-        animator.SetFloat("Angle", angle_du_vect);
-
-
     }
 }
