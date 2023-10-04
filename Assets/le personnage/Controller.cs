77deb751c5cdc7e6c216d741f0b191ab3148f96b
PaDideee <camilleTV.11@gmail.com>
2023-10-04 14:14:15 +0200

    bdv

--- Assets/le personnage/Controller.cs
+++ Assets/le personnage/Controller.cs
@@ -8,6 +8,7 @@ public class Controller : MonoBehaviour
 {
 
     public Rigidbody2D rb2d;
+    public Animator animator;
     public float speed = 5;
 
     // Start is called before the first frame update
@@ -19,10 +20,14 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal") * speed;
-        float V = Input.GetAxis("Vertical") * speed;
+        float h = Input.GetAxis("Horizontal");
+        float V = Input.GetAxis("Vertical");
+        float angle_du_vect = Mathf.Atan2(h, V) * Mathf.Rad2Deg / 360;
+        angle_du_vect = angle_du_vect + 0.5f;
 
-        rb2d.velocity = new Vector2(h, V);
-  
+        rb2d.velocity = new Vector2(h, V).normalized * speed;
+        animator.SetFloat("Angle", angle_du_vect);
+
+
     }
 }
